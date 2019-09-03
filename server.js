e25e4bfad858494e1a60a7086863a542af282e88
bouzmitwidad <bouzmitwidad7@gmail.com>
2019-09-03 10:56:46 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require('./models/db');
+
+const express = require('express');
+const path = require ('path');
+const exphbs=require ('express-handlebars');
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+var app = express();
+//use function for middleware
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+//convert to format json 
+app.use(bodyparser.json());
+
+//configure le template handlebars 
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('Express server started at port : 3000');
+});
+
+app.use('/employee', employeeController);
